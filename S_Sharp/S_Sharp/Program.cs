884cf722bb4ebf5c64918821715858daaedcdd27
DESKTOP-92KNQDD\lider <lider.x2011@yandex.ru>
2022-02-16 18:27:58 +0300

    OOP

--- S_Sharp/S_Sharp/Program.cs
+++ S_Sharp/S_Sharp/Program.cs
@@ -1,12 +1,64 @@
 ﻿using System;
 
-namespace S_Sharp
+namespace C_Sharp
 {
-    internal class Program
+    //Инкапсуляция
+    public class Gun
+    {
+        public bool amo = false;
+        private void Reloud()
+        {
+            Console.WriteLine("Я пустой");
+            Console.WriteLine("Перезарезаю");
+            amo = true;
+        }
+        public void Sooht()
+        {
+            while (amo == false)
+            {
+                Reloud();
+                continue;
+            }
+            Console.WriteLine("Выстрел");
+            amo = false;
+        }
+    }
+    //END
+
+    //Наследование и Полифорфизм
+    public class AutoGun : Gun
+    {
+        new public bool amo = false;
+        private void Reloud()
+        {
+            Console.WriteLine("Я пустой");
+            Console.WriteLine("Перезарезаю");
+            amo = true;
+        }
+       new public void Sooht()
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                while (amo == false)
+                {
+                    Reloud();
+                    continue;
+                }
+                Console.WriteLine($"{i} Выстрел");
+                
+                amo = false;
+            }
+        }
+    }
+    //END
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Gun gun = new Gun();
+            gun.Sooht();
+            AutoGun gunAuto = new AutoGun();
+            gunAuto.Sooht();
         }
     }
 }
