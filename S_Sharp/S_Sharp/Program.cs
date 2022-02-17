c24ccc66357f099cbb7d25b07ec1d3665a8403e9
DESKTOP-92KNQDD\lider <lider.x2011@yandex.ru>
2022-02-18 00:59:55 +0300

    cycle & operator

--- S_Sharp/S_Sharp/Program.cs
+++ S_Sharp/S_Sharp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections;
+using System;
 
 namespace C_Sharp
 {
@@ -75,8 +76,101 @@ namespace C_Sharp
             Console.WriteLine($"Выстрел/сек {sh_sec} Материал {material}");
         }
     }
-    //Циклы
+    //END
 
+    //4.Циклы
+    public class Cikle
+    {
+        protected string[] mas = { "l", "u", "c", "k" };
+        private int j = 1;
+        private int k = 3;
+        private int m = 5;
+        public void aFor()
+        {
+            for (int i = j; i < 5; i++)
+            {
+                Console.WriteLine($"for {i}");
+            }
+            aDoWhile();
+        }
+        public void aDoWhile()
+        {
+            do
+            {
+                Console.WriteLine($"Do...While {k}");
+                k--;
+            }
+            while (k > 0);
+            aWhile();
+        }
+        public void aWhile()
+        {
+            while (m == 5)
+            {
+                Console.WriteLine($"While {m}");
+                m--;
+            }
+            aForeach();
+
+        }
+        public void aForeach()
+        {
+            foreach (string s in mas)
+            {
+                Console.WriteLine($"Foreach {s}");
+            }
+        }
+
+    }
+    //END
+
+    //4.1Enumerable
+    class Week : IEnumerable
+    {
+        string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday",
+                         "Friday", "Saturday", "Sunday" };
+        public IEnumerator GetEnumerator() => days.GetEnumerator();
+    }
+    //END
+
+    //5.Operator
+    class Operator
+    {
+        public int a =45 ;
+        private int b = 45;
+        private int c = -45;
+        public void Oper()
+        {
+            if(a<50)
+            {
+                int z = a + b;
+                Console.WriteLine(z);
+            }
+            else if (a<-1)
+            {
+                int z = a + c;
+                Console.WriteLine(z);
+            }
+            else
+            {
+                switch (a)
+                {
+                    case (60):
+                        Console.WriteLine("a = 60");
+                        break;
+                    case (70):
+                        Console.WriteLine("a = 70");
+                        break;
+                    default:
+                        Console.WriteLine("Не повезло");
+                        break;
+                }
+
+            }
+        }
+
+    }
+    //END
     class Program
     {
         static void Main(string[] args)
@@ -91,8 +185,10 @@ namespace C_Sharp
 
             AutoGun gunAuto = new AutoGun();
             gunAuto.Sooht();
+
             //3.
             Console.WriteLine("_");
+
             MP_40 mp = new MP_40();
             mp.material = "iron";
             mp.Shoot_MP();
@@ -101,7 +197,27 @@ namespace C_Sharp
             ak.amo = 45;
             ak.material = "Polimer & steel";
             ak.Shoot_AK();
-            
+
+            //4.
+            Console.WriteLine("_");
+
+            Cikle cikle = new Cikle();
+            cikle.aFor();
+            Week week = new Week();
+            foreach(var day in week)
+            {
+                Console.WriteLine(day);
+            }
+
+            //5.
+            Console.WriteLine("_");
+
+            Operator oper = new Operator();
+            oper.Oper();
+            oper.a = -100;
+            oper.Oper();
+            oper.a = 100;
+            oper.Oper();
 
         }
     }
